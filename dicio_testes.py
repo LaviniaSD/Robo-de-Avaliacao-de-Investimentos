49e898a33bd12561d76efa0620a359f3c47a63cd
Lavínia Dias <103752863+LaviniaSD@users.noreply.github.com>
2022-05-30 09:42:52 -0300

    Crie dicio_testes.py
    Cria dicionários para testar o código que cria a planilha referente à carteira

---
+++ dicio_testes.py
@@ -0,0 +1,4 @@
+{'JPY': 5000.0, 'GBP': 3550.0, 'CAD': 1400.0} #dicionario moeda com a quantidade
+{'PINS': 100.0, 'VALE': 274.0, 'NFLX': 20.0} #dicionario acao com quantidade
+{'JPY': 0.04, 'GBP': 5.95, 'CAD': 3.71}#dicionario cotacao moedda em relação ao real
+{'PINS': 96.11, 'VALE': 85.02, 'NFLX': 917.39}#dicionario cotacao acao em relação ao real
