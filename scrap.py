60384234fadf07dae1afad46c01bd8824ad8a392
Lavínia Dias <103752863+LaviniaSD@users.noreply.github.com>
2022-06-07 14:22:31 -0300

    Corrija o código para tratar exceções

--- scrap.py
+++ scrap.py
@@ -1,41 +1,40 @@
-#Bibliotecas necessárias
+#bibliotecas necessárias para o web scraping
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-#Função que pede url e pega seu html
-def pegue_site():
+
+def pegue_site(): # Lê a url inserida pelo usuário e retorna o html do site
     url = input("Insira aqui sua url: ")
-    html = urlopen(url)
-    html_lido= BeautifulSoup(html, 'html.parser')
-    return html_lido
-    
-#transforma o valor retornado da função pegue_site em uma variável
-html_lido = pegue_site()
+    try:
+        html = urlopen(url)
+        html_lido= BeautifulSoup(html, 'html.parser')
+    except : 
+        print('Esse não é um site, tente novamente.')
+    else:
+        return html_lido
 
-#Pega a tabela na div de classe 'moeda' do html e retorna um dicionário com as moedas e suas quantidades 
-def dic_moedas(hipertext):
-    div_moeda = hipertext.find_all("div", {"class": "moeda"})
-    for moeda in div_moeda:
-        linhas_moeda = moeda.find_all('tr')
-    dicionario_moedas = {}
-    for linha in linhas_moeda:
-        children = linha.findChildren("td")
-        if children == []:
-            children == []
-        else:
-            dicionario_moedas[children[0].text] = int(children[1].text)
-    return dicionario_moedas
 
-#Pega a tabela na div de classe 'acao' do html e retorna um dicionário com as ações e suas quantidades 
-def dic_acoes(hipertext):
-    div_acoes = hipertext.find_all("div", {"class": "acao"})
-    for acao in div_acoes:
-        linhas_acoes = acao.find_all('tr')
-    dicionario_acoes = {}
-    for linha in linhas_acoes:
-        children = linha.findChildren("td")
-        if children == []:
-            children == []
-        else:
-            dicionario_acoes[children[0].text] = int(children[1].text)
-    return dicionario_acoes
+
+
+
+
+def dic_moedas(hipertext): #Acha no html a classe moeda e retorna um dicionário com as informações que estão em uma tabela dentro da classe
+    try:
+        div_moeda = hipertext.find_all("div", {"class": "moeda"})
+        for moeda in div_moeda:
+            linhas_moeda = moeda.find_all('tr')
+        dicionario_moedas = {}
+        for linha in linhas_moeda:
+            children = linha.findChildren("td")
+            if children == []:
+                children == []
+            else:
+                dicionario_moedas[children[0].text] = float(children[1].text)
+    except:
+        print('Não foi possível achar moedas nessa carteira')
+    else:
+        return dicionario_moedas
+
+
+
+     
