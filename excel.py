256363de3893b3d40d612bff28518c171cf87f1d
GeorgeRV <48179995+GeorgeRV@users.noreply.github.com>
2022-06-08 13:02:15 -0300

    Atualize excel.py
    Crie as funções dados e redimensionar e mude a disposição dos dados na planilha

--- excel.py
+++ excel.py
@@ -1,31 +1,48 @@
 import openpyxl
+from openpyxl.utils import get_column_letter
+
+
+def dados(ws, ativo, cota, col):
+
+    linha = 2
+    for nome in ativo.keys():
+        quant = ativo[nome]
+        cotacao = quant * cota[nome]
+
+        ws.cell(column=col, row=linha, value=nome)
+        ws.cell(column=col+1, row=linha, value=quant)
+        ws.cell(column=col+2, row=linha, value=cotacao).number_format = 'R$#,##0.00'
+
+        linha += 1
+    return ws
+
+
+def redimensionar(ws):
+
+    dims = {}
+    for row in ws.rows:
+        for cell in row:
+            if cell.value:
+                dims[cell.column_letter] = max((dims.get(cell.column_letter, 0), len(str(cell.value))))
+    for col, value in dims.items():
+        ws.column_dimensions[col].width = value + 10
+    return ws
+
 
 def planilha(moedas, acoes, moedas_cota, acoes_cota):
+
     wb = openpyxl.Workbook()
 
     ws1 = wb.active
     ws1.title = "Carteira"
 
+    ws1.append(["Moedas", "Quantidade", "Cotação", "", "Ações", "Quantidade", "Cotação"])
 
-    ws1.append(["Moedas", "Quantidade", "Cotação"])
+    ws1 = dados(ws1, moedas, moedas_cota, 1)
+    ws1 = dados(ws1, acoes, acoes_cota, 5)
 
+    # Redimensionando as celas
+    ws1 = redimensionar(ws1)
 
-    for moeda, quant_moeda in moedas.items():
-        #print('moeda', moeda, "qmoeda",quant_moeda)
-        cota  = quant_moeda * moedas_cota[moeda]
-        linha = (moeda, quant_moeda, cota)
-        ws1.append(linha)
-        
-    ws1.append([""])
-    ws1.append(["Ações", "Quantidade", "Cotação"])
-
-    for acao, quant_acao in acoes.items():
-        #print('acao',acao, "quant_acao",quant_acao)
-        cota  = quant_acao * acoes_cota[acao]
-        linha = (acao, quant_acao, cota)
-        ws1.append(linha)
-
-    dest_filename = input("Digite o nome  de arquivo: ")+ '.xlsx'
-    wb.save(filename = dest_filename)
-
-
+    dest_filename = input("Digite o nome  de arquivo: ")+'.xlsx'
+    wb.save(filename=dest_filename)
