0bf0b8ec47bc41ca99fa5437bd1538f7bff02e00
Lavínia Dias <103752863+LaviniaSD@users.noreply.github.com>
2022-06-14 16:16:30 -0300

    Crie a função qr()
    Crie a função que insere o qrcode com o valor total na planilha do excel

--- excel.py
+++ excel.py
@@ -1,3 +1,6 @@
+from openpyxl import load_workbook
+import qrcode
+
 
 def valor_total(moedas, acoes, moedas_cota, acoes_cota):
     soma=0
@@ -7,3 +10,14 @@ def valor_total(moedas, acoes, moedas_cota, acoes_cota):
     for acao, quant_acao in acoes.items():
         soma += quant_acao * acoes_cota[acao]
     return soma
+
+def qr(soma):
+    wb = opx.load_workbook(filename="carteira.xlsx")
+    ws = wb.create_sheet(title="QrCode")
+    qr = qrcode.QRCode(version = 1, error_correction = qrcode.constants.ERROR_CORRECT_H,box_size = 10, border = 4)
+    qr.add_data(soma)
+    img=qr.make_image()
+    img.save('total.png')
+    img = opx.drawing.image.Image('total.png')
+    ws.add_image(img, "A1")
+    wb.save(filename="carteira.xlsx")
