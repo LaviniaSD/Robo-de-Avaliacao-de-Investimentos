cc230b0757f4dd034631c8c6b926c5fdb90a99c0
Lavínia <L4viniadias510@gmail.com>
2022-06-18 11:36:48 -0300

    Crie a funcao graficos_acoes

--- pacote_a2_ic/graficos_historicos.py
+++ pacote_a2_ic/graficos_historicos.py
@@ -7,34 +7,52 @@ import os
 from datetime import datetime
 
 
-def graficos(dicionario_cotacoes_moedas, dicionario_preco_acoes):
-    wb = opx.load_workbook(filename= 'Carteira.xlsx' )
+def graficos_acoes(dicionario_historico_acoes):
+    wb = opx.load_workbook(filename='BANCO_GLLYT.xlsx')
     contador = 0
-    for moeda in dicionario_cotacoes_moedas.keys():
-        print(moeda)
+    for acao in dicionario_historico_acoes.keys():
         if contador == 3:
             break
         else:
-            historico = dicionario_cotacoes_moedas.get(moeda)
-            print(historico)
+            historico = dicionario_historico_acoes.get(acao)
+            try:
+                fig = go.Figure()
+                fig.add_trace(go.Candlestick(x=historico.index,open = historico['Open'], high=historico['High'], low=historico['Low'], close=historico['Close']))
+                fig.update_layout(title = f'Variação de {acao} ao longo do tempo')
+                fig.write_image(f'grafico_acao{contador}.jpg')
+                img = opx.drawing.image.Image(f'grafico_acao{contador}.jpg')
+                ws = wb.create_sheet(title=f"Gráficos_ações{contador}")
+                ws.add_image(img, "A1")
+            except:
+                historico = historico
+        contador+=1
+    wb.save(filename='BANCO_GLLYT.xlsx' )
+
+
+
+
+
+
+
+def graficos_moedas(dicionario_historico_moedas):
+    wb = opx.load_workbook(filename='BANCO_GLLYT.xlsx')
+    contador = 0
+    for moeda in dicionario_historico_moedas.keys():
+        if contador == 3:
+            break
+        else:
+            historico = dicionario_historico_moedas.get(moeda)
             try:
                 fig = go.Figure()
                 fig.add_trace(go.Candlestick(x=historico.index,open = historico['Open'], high=historico['High'], low=historico['Low'], close=historico['Close']))
                 fig.update_layout(title = f'Variação de {moeda} ao longo do tempo')
                 fig.write_image(f'grafico_moeda{contador}.jpg')
                 img = opx.drawing.image.Image(f'grafico_moeda{contador}.jpg')
-                ws = wb.create_sheet(title=f"Gráficos_{contador}")
+                ws = wb.create_sheet(title=f"Gráficos_moedas{contador}")
                 ws.add_image(img, "A1")
             except:
                 historico = historico
         contador+=1
-    wb.save(filename='Carteira.xlsx' )
+    wb.save(filename='BANCO_GLLYT.xlsx' )
     
-'''                
-    for historico in dicionario_preco_acoes.values():
-        fig = go.Figure()
-        fig.add_trace(go.Candlestick(x=historico.index,open = historico['Open'], high=historico['High'], low=historico['Low'], close=historico['Close'], name = 'market data'))
-        fig.update_layout(title = ' Google share price', yaxis_title = 'Stock Price (USD)')
-        fig.write_image('graficos_acoes.jpg')              
-'''
 
