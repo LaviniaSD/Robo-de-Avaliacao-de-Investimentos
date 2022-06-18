c7e152eeb9b21b569a57b2ef11d1c8c2ed179a55
Lavínia <L4viniadias510@gmail.com>
2022-06-18 01:45:42 -0300

    Conserte a função graficos

--- graficos_historicos.py
+++ graficos_historicos.py
@@ -1,31 +1,43 @@
 import openpyxl as opx
-
-wb = opx.Workbook()
-
-### Histórico das moedas
-ws_moedas = wb.active
-ws_moedas.title = "Histórico_moedas"
-
-ws_moedas["A1"] = "Histórico das moedas"
-# pd.from_dict()
-df_moedas = pd.DataFrame(dicionario_historico_cotacoes_moedas)
-
-dados_moedas = opx.chart.Reference(df_moedas) # Referencia o dataframe do histórico das moedas
-graf_hist_moedas = opx.chart.LineChart()
-graf_hist_moedas.add_data(dados_moedas)
-ws_moedas.add_chart(graf_hist_moedas, "F1") # Posição do gráfico
+from openpyxl import load_workbook
+import plotly.graph_objs as go
+import qrcode
+import pandas as pd
+import os
+from datetime import datetime
 
 
-### Histórico das ações
-ws_acoes = wb.create_sheet(title="Histórico_ações")
+def graficos(dicionario_cotacoes_moedas, dicionario_preco_acoes):
+    wb = opx.load_workbook(filename= 'Carteira.xlsx' )
+    contador = 0
+    for moeda in dicionario_cotacoes_moedas.keys():
+        print(moeda)
+        if contador == 3:
+            break
+        else:
+            historico = dicionario_cotacoes_moedas.get(moeda)
+            print(historico)
+            try:
+                fig = go.Figure(data=[go.Candlestick(x=historico['Date'],
+                open=historico['Open'],
+                high=historico['High'],
+                low=historico['Low'],
+                close=historico['Close'])])
 
-ws_acoes["A1"] = "Histórico das ações"
-df_acoes = pd.DataFrame(dicionario_historico_preco_acoes)
+                fig.update_layout(title = f'Variação de {moeda} ao longo do tempo')
+                fig.write_image(f'grafico_moeda{contador}.jpg')
+                img = opx.drawing.image.Image(f'grafico_moeda{contador}.jpg')
+                ws = wb.create_sheet(title=f"Gráficos_{contador}")
+                ws.add_image(img, "A1")
+                wb.save(filename='Carteira.xlsx' )
+            except:
+                historico = historico
+    contador+=1
 
-dados_acoes = opx.chart.Reference(df_acoes) # Referencia o dataframe do histórico das ações
-graf_hist_acoes = opx.chart.LineChart()
-graf_hist_acoes.add_data(dados_acoes)
-ws_acoes.add_chart(graf_hist_acoes, "F1") # Posição do gráfico
-
-
-wb.save("Gráficos_do_Histórico.xlsx")
+'''                
+    for historico in dicionario_preco_acoes.values():
+        fig = go.Figure()
+        fig.add_trace(go.Candlestick(x=historico.index,open = historico['Open'], high=historico['High'], low=historico['Low'], close=historico['Close'], name = 'market data'))
+        fig.update_layout(title = ' Google share price', yaxis_title = 'Stock Price (USD)')
+        fig.write_image('graficos_acoes.jpg')              
+'''
---
+++ graficos_historicos.py.bak
@@ -0,0 +1,31 @@
+import openpyxl as opx
+
+wb = opx.Workbook()
+
+### Histórico das moedas
+ws_moedas = wb.active
+ws_moedas.title = "Histórico_moedas"
+
+ws_moedas["A1"] = "Histórico das moedas"
+# pd.from_dict()
+df_moedas = pd.DataFrame(dicionario_historico_cotacoes_moedas)
+
+dados_moedas = opx.chart.Reference(df_moedas) # Referencia o dataframe do histórico das moedas
+graf_hist_moedas = opx.chart.LineChart()
+graf_hist_moedas.add_data(dados_moedas)
+ws_moedas.add_chart(graf_hist_moedas, "F1") # Posição do gráfico
+
+
+### Histórico das ações
+ws_acoes = wb.create_sheet(title="Histórico_ações")
+
+ws_acoes["A1"] = "Histórico das ações"
+df_acoes = pd.DataFrame(dicionario_historico_preco_acoes)
+
+dados_acoes = opx.chart.Reference(df_acoes) # Referencia o dataframe do histórico das ações
+graf_hist_acoes = opx.chart.LineChart()
+graf_hist_acoes.add_data(dados_acoes)
+ws_acoes.add_chart(graf_hist_acoes, "F1") # Posição do gráfico
+
+
+wb.save("Gráficos_do_Histórico.xlsx")
